a2c14dbd3b5e70467239952f6e65ced716f2aceb
nrtonchev <nikolaytonchev.89@gmail.com>
2025-01-11 18:01:57 +0200

    Initial commit

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const appIdKey = import.meta.env.OPEN_WEATHER_KEY;
+const appIdKey = import.meta.env.VITE_OPEN_WEATHER_KEY;
 const url = 'https://api.openweathermap.org/data/2.5/weather?'
 
 const getCapitalWeather = (lat, lon) => {
+  console.log(appIdKey);
   const request = axios.get(`${url}lat=${lat}&lon=${lon}&appid=${appIdKey}&units=metric`);
   return request.then(response => response.data);
 }
